e7169bfb2678f885ba2b98672a336a8dfd4c4695
frcharry <fabio.charry@gmail.com>
2019-04-15 22:21:59 -0500

    segundo query

--- solution.rb
+++ solution.rb
@@ -17,3 +17,9 @@ get '/' do
     end
   end
 end
+
+get '/makers/:nombre' do
+  <<-HTML
+    <h1>"Hola #{params[:nombre].capitalize}!"</h1>
+  HTML
+end
--- solution_makeitreal.rb
+++
@@ -1,7 +0,0 @@
-get '/' do
-  if params[:nombre].present?
-    "<h1>Hola #{params[:nombre].capitalize}!</h1>"
-  else
-    "<h1>Hola desconocido!</h1>"
-  end
-end
